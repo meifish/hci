bb0a002044f37fe7e97420361c6908f02a906dbc
Meiyu Chen <meifish.kat@gmail.com>
2020-11-25 21:21:48 -0500

    Add controler to elevator summon

--- lib/Pages/UseElevatorPage.dart
+++ lib/Pages/UseElevatorPage.dart
@@ -6,6 +6,8 @@ class UseElevatorPage extends StatefulWidget {
   String floor = "44";
   String up_img = "assets/images/up_1.png";
   String down_img = "assets/images/down_1.png";
+  String dropdownValue = "44";
+  int slideval = 44;
 
   @override
   _UseElevatorPageState createState() => _UseElevatorPageState();
@@ -29,11 +31,67 @@ class _UseElevatorPageState extends State<UseElevatorPage> {
                     fit: BoxFit.cover)),
             child: Stack(children: [
               Positioned(
+                  top: 40,
+                  left: 40,
+                  child: Row(children: [
+                    Text(
+                      "Change Floor",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    SizedBox(width: 30),
+                    DropdownButton<String>(
+                      value: widget.floor,
+                      iconSize: 40,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.black, fontSize: 40),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          widget.dropdownValue = newValue;
+                          widget.floor = newValue;
+                        });
+                      },
+                      items: List<int>.generate(50, (i) => i + 1)
+                          .map<DropdownMenuItem<String>>((int value) {
+                        return DropdownMenuItem<String>(
+                          value: value.toString(),
+                          child: Text(value.toString()),
+                        );
+                      }).toList(),
+                    ),
+                  ])),
+              Positioned(
                   left: SizeConfig.screenWidth / 3 + 10,
                   bottom: SizeConfig.screenHeight / 3 + 5,
-                  child: Text(widget.floor,
-                      style: TextStyle(
-                          fontSize: 100, fontWeight: FontWeight.bold))),
+                  child: InkWell(
+                    child: Text(widget.floor,
+                        style: TextStyle(
+                            fontSize: 100, fontWeight: FontWeight.bold)),
+                    onTap: () {},
+                  )),
+              Positioned(
+                  top: 150,
+                  left: 180,
+                  child: RotatedBox(
+                    quarterTurns: 1,
+                    child: Container(
+                      width: 300,
+                      child: Slider(
+                        value: double.parse(widget.floor),
+                        min: 1,
+                        max: 50,
+                        divisions: 50,
+                        activeColor: Colors.cyan,
+                        inactiveColor: Colors.grey,
+                        // label: widget.slideval.toString(),
+                        onChanged: (double newValue) {
+                          setState(() {
+                            widget.slideval = newValue.round();
+                            widget.floor = newValue.toInt().toString();
+                          });
+                        },
+                      ),
+                    ),
+                  )),
               Positioned(
                   left: 30,
                   bottom: 10,
